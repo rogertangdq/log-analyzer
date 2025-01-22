2e0aa0c7ec0325df45c752aa2f15f7499f4c6d34
Roger Tang <rogertangdq@gmail.com>
2025-01-21 16:42:44 -0800

    refactor classes for readability

--- src/main/java/org/cyberelay/loganalyzer/LogAnalyzerApplication.java
+++ src/main/java/org/cyberelay/loganalyzer/LogAnalyzerApplication.java
@@ -1,27 +1,14 @@
 package org.cyberelay.loganalyzer;
 
-import com.google.common.collect.HashMultimap;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Reader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
 
 @SpringBootApplication
 public class LogAnalyzerApplication implements CommandLineRunner {
@@ -31,7 +18,7 @@ public class LogAnalyzerApplication implements CommandLineRunner {
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(String... args) {
 		var options = new Options();
 		var input = Option.builder("i")
 				.longOpt("input")
@@ -62,128 +49,13 @@ public class LogAnalyzerApplication implements CommandLineRunner {
 			String inputFilePath = cmd.getOptionValue("input", "log.txt");
 			String outputFilePath = cmd.getOptionValue("output", "output.txt");
 			String lookupFilePath = cmd.getOptionValue("lookup", "lookup.csv");
-			var lookupTable = parseLookupTable(lookupFilePath);
-			var collector = new LogStats(lookupTable);
-			collectStats(inputFilePath, collector);
-			collector.output(outputFilePath);
+
+			// parse the input/lookup data files, generate stats and write them into output file
+			new LogStatsCollector(lookupFilePath).collect(inputFilePath, outputFilePath);
 		} catch (ParseException e) {
 			System.out.println(e.getMessage());
 			formatter.printHelp("Log-analyzer", options);
-
 			System.exit(1);
-		}
-	}
-
-	private static void collectStats(String logFilePath, LogStats logStatsCollector) {
-		try (Reader reader = new FileReader(logFilePath)) {
-			CSVFormat.DEFAULT.builder()
-					.setHeader()
-					.setSkipHeaderRecord(true) // The header row is skipped
-					.build()
-					.parse(reader)
-					.forEach(record -> {
-						var port = Optional
-								.ofNullable(record.get("dstport"))
-								.orElseThrow(() -> new RuntimeException("dstport column not found in " + logFilePath));
-						var protocol = Optional
-								.ofNullable(record.get("protocol"))
-								.orElseThrow(() -> new RuntimeException("protocol column not found in " + logFilePath));
-						logStatsCollector.increment(new Socket(port, protocol));
-					});
-		} catch (Exception e) {
-			throw new RuntimeException("Log file error: " + e.getMessage(), e);
-		}
-	}
-
-	private static HashMultimap<Socket, String> parseLookupTable(String filePath) {
-		HashMultimap<Socket, String> result = HashMultimap.create();
-
-		try (Reader reader = new FileReader(filePath)) {
-			CSVFormat.DEFAULT.builder()
-					.setHeader()
-					.setSkipHeaderRecord(true) // Optional: skip the header row
-					.build()
-					.parse(reader)
-					.forEach(record -> {
-						var port = Optional
-								.ofNullable(record.get("dstport"))
-								.orElseThrow(() -> new RuntimeException("dstport column not found in " + filePath));
-						var protocol = Optional
-								.ofNullable(record.get("protocol"))
-								.orElseThrow(() -> new RuntimeException("protocol column not found in " + filePath));
-						var tag = Optional
-								.ofNullable(record.get("tag"))
-								.orElseThrow(() -> new RuntimeException("tag column not found in " + filePath));
-						var entry = new Socket(port, protocol);
-
-						result.put(entry, tag);
-					});
-		} catch (Exception e) {
-			throw new RuntimeException("Lookup table file error: " + e.getMessage(), e);
-		}
-
-		return result;
-	}
-
-	record Socket(String port, String protocol) {}
-
-	static class LogStats {
-		final Map<Socket, Integer> statsBySocket;
-		final Map<String, Integer> statsByTag;
-		final HashMultimap<Socket, String> lookupTable;
-
-		LogStats(HashMultimap<Socket, String> lookupTable) {
-			this.statsBySocket = new HashMap<>();
-			this.statsByTag = new HashMap<>();
-			this.lookupTable = lookupTable;
-		}
-
-		void increment(Socket socket) {
-			var socketCount = statsBySocket.computeIfAbsent(socket, key -> 0) + 1;
-			statsBySocket.put(socket, socketCount);
-
-			lookupTags(socket).forEach(tag -> {
-				var count = statsByTag.computeIfAbsent(tag, key -> 0) + 1;
-				statsByTag.put(tag, count);
-			});
-		}
-
-		void output(String filePath) {
-			try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-				// Write header 1
-				writer.write(padString("Tag") + padString("Count") + "\n");
-
-				statsByTag.forEach((tag, count) -> {
-                    try {
-                        writer.write(padString(tag) + padString(String.valueOf(count)) + "\n");
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-
-				writer.write("\n\n");
-				// Write header 2
-				writer.write(padString("Port") + padString("Protocol") + padString("Count") + "\n");
-
-				statsBySocket.forEach((socket, count) -> {
-					try {
-						writer.write(padString(socket.port) + padString(socket.protocol) + padString(String.valueOf(count)) + "\n");
-					} catch (IOException e) {
-						throw new RuntimeException(e);
-					}
-				});
-			} catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-		private String padString(String input) {
-			return StringUtils.rightPad(input, 15, ' ');
-		}
-
-		private Set<String> lookupTags(Socket key) {
-			var tags = lookupTable.get(key);
-			return tags.isEmpty() ? Set.of("untagged") : tags;
 		}
 	}
 }
---
+++ src/main/java/org/cyberelay/loganalyzer/LogStatsCollector.java
@@ -0,0 +1,135 @@
+package org.cyberelay.loganalyzer;
+
+import com.google.common.collect.HashMultimap;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class LogStatsCollector {
+    private record Socket(String port, String protocol) {}
+
+    private class Stats {
+        final Map<Socket, Integer> statsBySocket = new HashMap<>();
+        final Map<String, Integer> statsByTag = new HashMap<>();
+
+        void increment(Socket socket) {
+            var socketCount = statsBySocket.computeIfAbsent(socket, key -> 0) + 1;
+            statsBySocket.put(socket, socketCount);
+
+            lookupTags(socket).forEach(tag -> {
+                var count = statsByTag.computeIfAbsent(tag, key -> 0) + 1;
+                statsByTag.put(tag, count);
+            });
+        }
+    }
+
+    private final HashMultimap<Socket, String> lookupTable;
+
+    public LogStatsCollector(String lookupFilePath) {
+        this.lookupTable = parseLookupTable(lookupFilePath);
+    }
+
+    public void collect(String inputPath, String filePath) {
+        var stats = collectStats(inputPath);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            // Write header for Segment 1
+            writeline(writer, "Tag", "Count");
+            // Write data for Segment 1
+            stats.statsByTag.forEach((tag, count) -> writeline(writer, tag, String.valueOf(count)));
+
+            // Write an empty line as a separator between the first and second segment
+            writeline(writer,"");
+
+            // Write header for Segment 2
+            writeline(writer, "Port", "Protocol", "Count");
+            // Write data for Segment 2
+            stats.statsBySocket.forEach((socket, count) -> writeline(writer, socket.port, socket.protocol, String.valueOf(count)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void writeline(BufferedWriter writer, String... columns) {
+        try {
+            var line = Arrays.stream(columns).map(this::padString).collect(Collectors.joining());
+            writer.write(line);
+            writer.write("\n");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Stats collectStats(String logFilePath) {
+        var result = new Stats();
+        try (Reader reader = new FileReader(logFilePath)) {
+            CSVFormat.DEFAULT.builder()
+                    .setHeader()
+                    .setSkipHeaderRecord(true) // The header row is skipped
+                    .build()
+                    .parse(reader)
+                    .forEach(record -> {
+                        var port = Optional
+                                .ofNullable(record.get("dstport"))
+                                .orElseThrow(() -> new RuntimeException("dstport column not found in " + logFilePath));
+                        var protocol = Optional
+                                .ofNullable(record.get("protocol"))
+                                .orElseThrow(() -> new RuntimeException("protocol column not found in " + logFilePath));
+                        result.increment(new Socket(port, protocol));
+                    });
+            return result;
+        } catch (Exception e) {
+            throw new RuntimeException("Log file error: " + e.getMessage(), e);
+        }
+    }
+
+    private HashMultimap<Socket, String> parseLookupTable(String filePath) {
+        HashMultimap<Socket, String> result = HashMultimap.create();
+
+        try (Reader reader = new FileReader(filePath)) {
+            CSVFormat.DEFAULT.builder()
+                    .setHeader()
+                    .setSkipHeaderRecord(true) // Optional: skip the header row
+                    .build()
+                    .parse(reader)
+                    .forEach(record -> {
+                        var port = Optional
+                                .ofNullable(record.get("dstport"))
+                                .orElseThrow(() -> new RuntimeException("dstport column not found in " + filePath));
+                        var protocol = Optional
+                                .ofNullable(record.get("protocol"))
+                                .orElseThrow(() -> new RuntimeException("protocol column not found in " + filePath));
+                        var tag = Optional
+                                .ofNullable(record.get("tag"))
+                                .orElseThrow(() -> new RuntimeException("tag column not found in " + filePath));
+                        var entry = new Socket(port, protocol);
+
+                        result.put(entry, tag);
+                    });
+        } catch (Exception e) {
+            throw new RuntimeException("Lookup table file error: " + e.getMessage(), e);
+        }
+
+        return result;
+    }
+
+    private String padString(String input) {
+        return StringUtils.rightPad(input, 15, ' ');
+    }
+
+    private Set<String> lookupTags(Socket key) {
+        var tags = lookupTable.get(key);
+        return tags.isEmpty() ? Set.of("untagged") : tags;
+    }
+}
